1264a70c2f3230257d1d5125037602c759fffb3f
SeanChan0901 <SeanChan0901@gmail.com>
2020-05-17 22:43:53 +0800

    update

---
+++ 进程间通信/xsi/msg/proto.h
@@ -0,0 +1,22 @@
+// 通信协议
+// mac没有msg通信，在linux上重新写遍
+#ifndef _PROTO_H
+#define _PROTO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define KEYPATH "/etc/services"
+#define KEYPROJ 'g'  // 表示一个整形（一个ASCII码）
+#define NAMESIZE 1024
+
+struct msg_st {
+  // 数据传输格式
+  long mtype;
+  char name[NAMESIZE];
+  int math;
+  int chinese;
+};
+
+#endif
---
+++ 进程间通信/xsi/msg/rcver.c
@@ -0,0 +1,38 @@
+// 接收端
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+
+#include "proto.h"
+
+int main() {
+  key_t key;
+  int msgid;
+  struct msg_st rbuf;
+  key = ftok(KEYPATH, KEYPROJ);
+  if (key < 0) {
+    perror("ftok()");
+    exit(1);
+  }
+  msgid = msgget(key, IPC_CREAT | 0600);  // 一定要接收方先创建
+  if (msgid < 0) {
+    perror("msgget()");
+    exit(1);
+  }
+  while (1) {
+    // 需要接收的字节数可以用减法来确定
+    if (msgrcv(msgid, &rbuf, sizeof(rbuf) - sizeof(long), 0, 0) < 0) {
+      perror("msgrcv()");
+      exit(1);
+    }
+    // 接收信息
+    printf("NAME = %s\n", rbuf.name);
+    printf("MATH = %d\n", rbuf.math);
+    printf("CHINESE = %d\n", rbuf.chinese);
+  }
+  if (msgctl(msgid, IPC_RVID, NULL) < 0) {
+    perror("msgctl");
+    exit(1);
+  }
+  exit(0);
+}
