19b0a408759226c1cd7aa34d930d8641d210104e
SeanChan0901 <SeanChan0901@gmail.com>
2020-05-19 18:27:01 +0800

    update

--- 进程间通信/xsi/ftp/proto1.h
+++ 进程间通信/xsi/ftp/proto1.h
@@ -1,1 +1,36 @@
 // 实现ftp传输（用linux）
+#ifndef PROTO1_H
+#define PROTO1_H
+
+#define PATHMAX 1024
+#define DATAMAX 1024
+
+// 包的类型
+enum { MSG_PATH = 1, MSG_DATA, MSG_EOT };
+
+//  路径包
+typedef struct msg_path_st {
+  long mtpe;           // must be MSG_PATH
+  char path[PATHMAX];  // ASCIIz带尾0的串
+} msg_path_t;
+
+// 数据包
+typedef struct msg_data_st {
+  long mtype;  // must be MSG_DATA
+  char data[DATAMAX];
+  int datalen;
+} msg_data_t;
+
+// 结束符号包
+typedef struct msg_eot_st {
+  long mtype;  // must be MSG_EOT
+} msg_eot_t;
+
+// 需要收到才能知道是什么类型的data,用这个来封装
+union msg_s2c_un {
+  long mtype;
+  msg_data_t datamsg;
+  msg_eot_st eotmsg;
+};
+
+#endif
--- 进程间通信/xsi/semph/add_mp_semp.c
+++ 进程间通信/xsi/semph/add_mp_semp.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <errno.h>
 
 #define LINESIZE 1024
 #define FNAME "/tmp/out"
@@ -13,8 +14,35 @@
 // 多进程并发版本
 
 static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;  // 互斥量
+static int semid;
 
 // 竞争问题，同时竞争一个文件的写,实际上是一个数据写了20次
+
+static void P() {
+  struct sembuf op;
+  op.sem_num = 0;
+  op.sem_op = -1;
+  op.sem_flg = 0;
+  while (semop(semid, &op, 1) < 0) {
+    if (errno != EINTR || errno != EAGAIN) {
+      perror("semop()");
+      exit(1);
+    }
+  }
+}
+
+static void V() {
+  struct sembuf op;
+  op.sem_num = 0;
+  op.sem_op = 1;
+  op.sem_flg = 0;
+  while (semop(semid, &op, 1) < 0) {
+    if (errno != EINTR || errno != EAGAIN) {
+      perror("semop()");
+      exit(1);
+    }
+  }
+}
 
 static void* func_add(void* p) {
   FILE* fp;
@@ -31,19 +59,29 @@ static void* func_add(void* p) {
     exit(1);
   }
   //  锁加在临界区
-  lockf(fd, F_LOCK, 0);
+  P();
   fgets(linebuf, LINESIZE, fp);
   fseek(fp, 0, SEEK_SET);
   fprintf(fp, "%d\n",
           (atoi(linebuf) + 1));  // 从位置0开始写会依次覆盖掉后面的内容
   fflush(fp);
-  lockf(fd, F_ULOCK, 0);
+  V();
   fclose(fp);  // 防止意外解锁
   return NULL;
 }
 
 int main() {
   pid_t pid;
+  semid = semget(IPC_PRIVATE, 1, 0600);
+  if (semid < 0) {
+    perror("semget()");
+    exit(1);
+  }
+  semctl(semid, 0, SETVAL, 1);
+  if (semid < 0) {
+    perror("semctl()");
+    exit(1);
+  }
   for (int i = 0; i < PROCNUM; i++) {
     pid = fork();
     if (pid < 0) {
@@ -58,5 +96,6 @@ int main() {
   }
   // parent
   for (int i = 0; i < PROCNUM; i++) wait(NULL);
+  semctl(semid, 0, IPC_RMID);
   exit(0);
 }
